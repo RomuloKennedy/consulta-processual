c9da0a2da10361e74041dfd4d14fb3b33cfc6d59
Marcos Delgado <Marcos Delgado>
2024-07-18 23:13:53 -0300

    Adicionando mais-detalhes e colocando na rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MaisDetalhesComponent } from './mais-detalhes/mais-detalhes.component';
 
 export const routes: Routes = [
 
@@ -7,6 +8,11 @@ export const routes: Routes = [
         path: '',
         component: HomeComponent
     },
+
+    {
+        path:'mais-detalhes',
+        component: MaisDetalhesComponent
+    }
     
 
 ];
---
+++ src/app/mais-detalhes/mais-detalhes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mais-detalhes',
+  standalone: true,
+  imports: [],
+  templateUrl: './mais-detalhes.component.html',
+  styleUrl: './mais-detalhes.component.css'
+})
+export class MaisDetalhesComponent {
+
+}
